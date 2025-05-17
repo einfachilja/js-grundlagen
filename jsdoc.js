274c96b130474b40c3e88c347f21023c5c76c763
Ilja Völker-Savkiv <ilja@MacBookAir.fritz.box>
2025-05-17 12:46:36 +0200

    added jsdoc to jsdoc.js

---
+++ jsdoc.js
@@ -0,0 +1,11 @@
+
+/**
+ * This function is used to greet someone on the command line
+ * 
+ * @param {String} name - This is the name of the person that you want to greet
+ */
+
+function greet(name) {
+    console.log("Hallo" + name);
+    
+}
